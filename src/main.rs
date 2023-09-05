1ea0ca5e1b99e10b0f32615559c58416f798ff1b
Pagnany <eric.schmale@gmx.de>
2023-09-05 09:19:50 +0200

    read visual foxpro dbf file with odbc driver

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use encoding_rs::*;
+use odbc::*;
+use odbc_safe::AutocommitOn;
+
+fn main() {
+    env_logger::init();
+
+    match connect() {
+        Ok(()) => println!("Success"),
+        Err(diag) => println!("Error: {}", diag),
+    }
+}
+
+fn connect() -> std::result::Result<(), DiagnosticRecord> {
+    let env = create_environment_v3().map_err(|e| e.unwrap())?;
+
+    let buffer = r#"Driver={Microsoft Visual FoxPro Driver};SourceType=DBF;SourceDB=c:\test\;Exclusive=No;Collate=Machine;NULL=NO;DELETED=NO;BACKGROUNDFETCH=NO;"#;
+
+    let conn = env.connect_with_connection_string(&buffer)?;
+    execute_statement(&conn)
+}
+
+fn execute_statement<'env>(conn: &Connection<'env, AutocommitOn>) -> Result<()> {
+    let stmt = Statement::with_parent(conn)?;
+
+    let sql_text = "select * from Hallodatei".to_string();
+    let mut count = 0;
+
+    match stmt.exec_direct(&sql_text)? {
+        Data(mut stmt) => {
+            let cols = stmt.num_result_cols()?;
+            while let Some(mut cursor) = stmt.fetch()? {
+                for i in 1..=cols {
+                    let data = cursor.get_data::<Vec<u8>>(i as u16).unwrap().unwrap();
+                    let (result, _, _) = WINDOWS_1252.decode(&data);
+                    let s = result.to_string();
+                    println!("{}", s);
+                }
+
+                count += 1;
+            }
+        }
+        NoData(_) => println!("Query executed, no data returned"),
+    }
+    println!("Count: {}", count);
+
+    Ok(())
+}
